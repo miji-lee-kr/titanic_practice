c55dbb4d9eb8d3d42df18e990a6aae013cf2a369
Miji Lee <miji.lee.kr@hotmail.com>
2026-01-28 01:24:41 +0900

    producer consumer pattern practice

--- practice_advanced.py
+++ practice_advanced.py
@@ -1,38 +1,40 @@
-# race condition 
-# 
+# 생산자 소비자 패턴 producer consumer pattern
+# queue를 사이에 두고 producer와 consumer가 협업
+# data에 동시에 접근해서 망가질 수 있으니 queue에 담아놓은 걸 한 스레드가 접근해서 꺼내도록
 
+
+# producer 조건: 끝나기 전까지 / 
+# 
 import logging
-from concurrent.futures import ThreadPoolExecutor
+import concurrent.futures
 import time
 import threading
+import queue
+import random
 
 
-class FakeDataStore:
-    def __init__(self):
-        self.value = 0
-        self._lock = threading.Lock()
-
-    def update(self,n):
-        logging.info('Thread %s, %d: Starting update', n, self.value)
+# 데이터 만드는 생산자. (네트워크 대기상태 I/O 일 경우 등)
+def producer(q, e): # 쌓이면 queue는 자동으로 조절해준다는데 왜 while 조건이 필요?
+    while not e.is_set():
+        v = random.randint(1,5)
+        q.put(v)
+    
+# 데이터 받고 사용하는 소비자 (받아서 DB에 저장 등)
+def consumer(q, e):
+    while not e.is_set() or not q.empty():
+        num = q.get()
+        logging.info('Answer : %s, size = %d', num, q.qsize())
         
-        
-        with self._lock: # lock acquire 과 return 을 자동으로 해줌
-            logging.info('Thread %s acquires lock', n)
-            local_copy = self.value  
-            local_copy += 1 
-            time.sleep(0.1)
-            self.value = local_copy
-            logging.info('Thread %s: Finished update. Current value %d', n, self.value)
-        
-
-if __name__   == '__main__':
+    
+if __name__ == '__main__':
     format = '%(asctime)s: %(message)s'
     logging.basicConfig(format=format, level = logging.INFO, datefmt = '%H:%M:%S')
-    store = FakeDataStore()
-    logging.info('Testing update. Starting value %d', store.value)
-    
-    with ThreadPoolExecutor(max_workers= 3) as executor:
-        for n in ['1st', '2nd', '3rd']:
-            executor.submit(store.update, n)
-            
-    logging.info('Testing update. Ending value %d', store.result())
+    q = queue.Queue(maxsize=10)
+    e = threading.Event()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(producer, q,e) # q,e를 이 futures에도 공유하겠다
+        executor.submit(consumer, q,e)
+        time.sleep(1)
+        e.set()
+        
+        
