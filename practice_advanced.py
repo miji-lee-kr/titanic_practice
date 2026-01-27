fb5e0ec8a0154570be0e98b389131369d2b7c45d
Miji Lee <miji.lee.kr@hotmail.com>
2026-01-27 20:36:29 +0900

    race condition handling with class and lock

--- practice_advanced.py
+++ practice_advanced.py
@@ -1,28 +1,38 @@
-# CPU bound + sync
+# race condition 
+# 
 
+import logging
+from concurrent.futures import ThreadPoolExecutor
 import time
+import threading
 
 
-def cpu_bound(num):
-    return sum(i * i for i in range(num))
+class FakeDataStore:
+    def __init__(self):
+        self.value = 0
+        self._lock = threading.Lock()
 
+    def update(self,n):
+        logging.info('Thread %s, %d: Starting update', n, self.value)
+        
+        
+        with self._lock: # lock acquire 과 return 을 자동으로 해줌
+            logging.info('Thread %s acquires lock', n)
+            local_copy = self.value  
+            local_copy += 1 
+            time.sleep(0.1)
+            self.value = local_copy
+            logging.info('Thread %s: Finished update. Current value %d', n, self.value)
+        
 
-def find_sums(nums):
-    result = []
-    for num in nums:
-        result.append(cpu_bound(num))
-    return result
-
-def main():
-    nums= [3000000 + x for x in range(30)]
-    print(nums)
-    start_time = time.time()
-    total = find_sums(nums)
+if __name__   == '__main__':
+    format = '%(asctime)s: %(message)s'
+    logging.basicConfig(format=format, level = logging.INFO, datefmt = '%H:%M:%S')
+    store = FakeDataStore()
+    logging.info('Testing update. Starting value %d', store.value)
     
-    print(f'Total list: {total}')
-    print(f'Sum: {sum(total)}')
-    duration = time.time() - start_time
-    print(f'Duration: {duration} seconds')
-
-if __name__ == '__main__':
-    main()
+    with ThreadPoolExecutor(max_workers= 3) as executor:
+        for n in ['1st', '2nd', '3rd']:
+            executor.submit(store.update, n)
+            
+    logging.info('Testing update. Ending value %d', store.result())
