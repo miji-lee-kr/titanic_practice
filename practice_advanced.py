391c37d88ecf07fdcec34961a4f88a59b17f8280
Miji Lee <miji.lee.kr@hotmail.com>
2026-02-02 01:59:23 +0100

    queue with process

--- practice_advanced.py
+++ practice_advanced.py
@@ -1,40 +1,70 @@
-# 생산자 소비자 패턴 producer consumer pattern
-# queue를 사이에 두고 producer와 consumer가 협업
-# data에 동시에 접근해서 망가질 수 있으니 queue에 담아놓은 걸 한 스레드가 접근해서 꺼내도록
+# from multiprocessing import Process, current_process, Value, Array
+# import os
+# from concurrent.futures import ProcessPoolExecutor
+
+# shared_value = 0
+
+# def update_number(v):
+#     v = shared_value
+#     v.
 
 
-# producer 조건: 끝나기 전까지 / 
-# 
-import logging
-import concurrent.futures
+
+    
+# def main():
+#     process_id = os.getpid()     # 메인 프로세스 ID는 디버깅을 위해 필수
+#     print(f'Process ID {process_id}')
+#     with ProcessPoolExecutor(max_workers=3) as executor:
+#         futures = executor.submit(target = update_number) 
+#         futures.result()    
+#     print('Final data in original process', shared_value)
+
+        
+# if __name__ == '__main__':
+#     main()
+    
+    
+    
+    
+
+from multiprocessing import Process, Queue, current_process
 import time
-import threading
-import queue
-import random
+import os
+from concurrent.futures import ProcessPoolExecutor
 
 
-# 데이터 만드는 생산자. (네트워크 대기상태 I/O 일 경우 등)
-def producer(q, e): # 쌓이면 queue는 자동으로 조절해준다는데 왜 while 조건이 필요?
-    while not e.is_set():
-        v = random.randint(1,5)
-        q.put(v)
+
+def worker(q, num):
+    # 각자가 숫자 만든다.
+    # 그리고 하나에 넣느다
+    value= 0
+    for i in range(num):
+        value +=1
+    q.put(value)
+    q.put('Exit')
     
-# 데이터 받고 사용하는 소비자 (받아서 DB에 저장 등)
-def consumer(q, e):
-    while not e.is_set() or not q.empty():
-        num = q.get()
-        logging.info('Answer : %s, size = %d', num, q.qsize())
+def main():
+    # worker를 참고하는 processpool 만든다.
+    l = []
+    q = Queue()
+    for i in range(5):
+        p = Process(target = worker, name = i, args = (q,i))
+        l.append(p)
+    for j in l:
+        j.join()
         
-    
+        
+    # queue를 만든다
+    # queue에서 만든것을 빼서 더한다
+    #제동 걸어준다
+    exit_cnt = 0
+    total_cnt = 0
+    while exit_cnt <len(l):
+        if q.get() =='Exit':
+            exit_cnt +=1 
+        else: 
+            total_cnt += q.get() 
+        
+
 if __name__ == '__main__':
-    format = '%(asctime)s: %(message)s'
-    logging.basicConfig(format=format, level = logging.INFO, datefmt = '%H:%M:%S')
-    q = queue.Queue(maxsize=10)
-    e = threading.Event()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        executor.submit(producer, q,e) # q,e를 이 futures에도 공유하겠다
-        executor.submit(consumer, q,e)
-        time.sleep(1)
-        e.set()
-        
-        
+    main()
